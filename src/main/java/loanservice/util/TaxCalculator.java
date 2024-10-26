ce524c1c28f2d110a6e7f4d2bee26039b8f34c6e
Audinéia Carmo <56273265+audicarmo@users.noreply.github.com>
2024-10-26 18:31:34 -0300

    Rename age in TaxCalculator

--- src/main/java/loanservice/util/TaxCalculator.java
+++ src/main/java/loanservice/util/TaxCalculator.java
@@ -7,12 +7,12 @@ import java.math.BigDecimal;
 @Component
 public class TaxCalculator {
 
-    public BigDecimal calculateInterestRate(int idade) {
-        if (idade <= 25) {
+    public BigDecimal calculateInterestRate(int age) {
+        if (age <= 25) {
             return BigDecimal.valueOf(0.05);
-        } else if (idade <= 40) {
+        } else if (age <= 40) {
             return BigDecimal.valueOf(0.03);
-        } else if (idade <= 60) {
+        } else if (age <= 60) {
             return BigDecimal.valueOf(0.02);
         } else {
             return BigDecimal.valueOf(0.04);
